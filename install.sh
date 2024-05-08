b98156d74a46e9b6418d808b7c67486edb172555
GolDNenex <30295660+GolDNenex@users.noreply.github.com>
2024-05-08 12:21:21 +0200

    Update install.sh
    Edit install.sh to make it functional regardless of its location

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-sudo cp -r img/* /var/www/html/admin/img/
-sudo cp -r scripts/* /var/www/html/admin/scripts/
-sudo cp -r style/* /var/www/html/admin/style/
+#!/usr/bin/env bash
+
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+
+sudo cp -r $SCRIPT_DIR/img/* /var/www/html/admin/img/
+sudo cp -r $SCRIPT_DIR/scripts/* /var/www/html/admin/scripts/
+sudo cp -r $SCRIPT_DIR/style/* /var/www/html/admin/style/
 echo "Pi-hole dark-purple theme installed"
